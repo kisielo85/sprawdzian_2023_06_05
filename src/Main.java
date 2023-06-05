51f913c07fdb7806ed2ef0ff8dbff9f9d379b993
Przemek B <pprzemek89@gmail.com>
2023-06-05 12:19:32 +0200

    eee zapomniałem tego tutaj ten

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<NaPewnoZdam> chyba = new ArrayList<NaPewnoZdam>();
+
+        chyba.add(new NaPewnoZdam("Java",2,"Mirosław Zelent"));
+        chyba.add(new NaPewnoZdam("Chemia",6,"Walter White"));
+        chyba.add(new NaPewnoZdam("Polski",3,"Donald Trump"));
+
+        LocalDate date = LocalDate.of(2023, 6, 5);
+        chyba.add(new Zdalem("Matematyka",4,"RandomZYoutube", date));
+
+        for (NaPewnoZdam x: chyba) {
+            System.out.println(x.toString());
+        }
+        chyba.remove(0);
+
     }
 }
--- src/NaPewnoZdam.java
+++ src/NaPewnoZdam.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class NaPewnoZdam {
+public class NaPewnoZdam {
+    private String nazwaPrzedmiotu;
+    private int przewidywanaOcena;
+    private String nazwiskoNauczyciela;
+
+    public NaPewnoZdam(String nazwaPrzedmiotu, int przewidywanaOcena, String nazwiskoNauczyciela) {
+        this.nazwaPrzedmiotu = nazwaPrzedmiotu;
+        this.przewidywanaOcena = przewidywanaOcena;
+        this.nazwiskoNauczyciela = nazwiskoNauczyciela;
+    }
+
+    public String getNazwaPrzedmiotu() {
+        return nazwaPrzedmiotu;
+    }
+
+    public void setNazwaPrzedmiotu(String nazwaPrzedmiotu) {
+        this.nazwaPrzedmiotu = nazwaPrzedmiotu;
+    }
+
+    public int getPrzewidywanaOcena() {
+        return przewidywanaOcena;
+    }
+
+    public void setPrzewidywanaOcena(int przewidywanaOcena) {
+        this.przewidywanaOcena = przewidywanaOcena;
+    }
+
+    public String getNazwiskoNauczyciela() {
+        return nazwiskoNauczyciela;
+    }
+
+    public void setNazwiskoNauczyciela(String nazwiskoNauczyciela) {
+        this.nazwiskoNauczyciela = nazwiskoNauczyciela;
+    }
+
+    @Override
+    public String toString() {
+        return "Na pewno zdam z "+this.nazwaPrzedmiotu
+            +". Oczekuje dostać "+this.przewidywanaOcena
+            +". Nauczyciel "+this.nazwiskoNauczyciela+" jest najlepszy";
+    }
+
+
+    public void radosc(String tekst){
+        for (int i = 0; i<this.przewidywanaOcena; i++){
+            System.out.println(tekst);
+        }
+    }
 }
